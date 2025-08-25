dbab109fa1e42d265d809100308bf8be9a681d4a
Santosh Pattnaik <pattnaiksantosh231@gmail.com>
2025-08-25 19:17:53 +0530

    added a get route to list al person table contents

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Person;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json(Person::all());
+    }
 }
